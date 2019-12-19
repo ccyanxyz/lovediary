12415fd6a795f350f6b1d2780a4c331734d8f4f3
ccyanxyz <790266922@qq.com>
2019-12-20 01:53:11 +0800

    contract

--- contracts/contracts/LoveDiaryLogic_v0.sol
+++ contracts/contracts/LoveDiaryLogic_v0.sol
@@ -1,5 +1,89 @@
 pragma solidity >= 0.4.24;
 
 contract LogicV0 {
+	
+	// getters
+	function get_user(address addr) public view returns (User) {
+		return users[addr];
+	}
 
+	function get_invitations(address addr) public view returns (bytes32[]) {
+		return invitations[addr];
+	}
+
+	function get_channel(bytes32 cid) public view returns (Channel) {
+		return channels[cid]
+	}
+
+	// setters
+	function set_nickname(address addr, bytes nickname) public {
+		users[addr].nickname = nickname;
+	}
+
+	function set_status(address addr, bool status) public {
+		users[addr].status = status;
+	}
+
+	function set_sex(address addr, bool sex) public {
+		users[addr].sex = status;
+	}
+
+	function register_user(address addr, bytes nickname, bool status, bool sex) public {
+		users[addr].nickname = nickname;
+		users[addr].status = status;
+		users[addr].sex = sex;
+		users[addr].cid = 0x0;
+	}
+
+	// logic
+	// register channel
+	function register_channel(address user2, uint32 timestamp) public returns (bool) {
+		require(users[msg.sender].cid == 0x0);
+
+		Channel memory c;
+		c.user1 = msg.sender;
+		c.user2 = user2;
+		c.cid = keccak256(c.user1 + c.user2);
+		c.status = false;
+		c.timestamp = timestamp;
+
+		channels[c.cid] = c;
+		users[msg.sender].cid = c.cid;
+
+		invitations[c.user2].push(c.cid);
+		return true;
+	}
+
+	// delete inactive channel(not approved by user2)
+	function delete_channel(bytes32 cid) public returns (bool) {
+		require(channels[cid].status = false);
+		delete channels[cid];
+		users[msg.sender].cid = 0x0;
+		return true;
+	}
+
+	// approve invitation
+	function approve_channel(bytes32 cid) public returns (bool) {
+		require(channels[cid].user2 == msg.sender && users[msg.sender].cid == 0x0);
+
+		channels[cid].status = true;
+		users[msg.sender].cid = cid;
+		delete invitations[msg.sender];
+		return true;
+	}
+
+	// send message
+	function send_msg(bytes multihash, uint32 timestamp, uint type) public returns (bool) {
+		require(users[msg.sender].cid != 0x0);
+
+		Message memory m;
+		m.sender = msg.sender;
+		m.multihash = multihash;
+		m.timestamp = timestamp;
+		m.type = type;
+		m.mid = keccak256(m.sender + m.multihash + m.timestamp);
+
+		msgs[mid] = m;
+		return true;
+	}
 }
--- contracts/contracts/LoveDiaryStorage.sol
+++ contracts/contracts/LoveDiaryStorage.sol
@@ -1,6 +1,35 @@
 pragma solidity >=0.4.24;
 
 contract Storage is OwnedUpgradeabilityProxy {
-	// contract storage
+	// message in channel or friend request message
+	struct Message {
+		address sender;
+		bytes multihash;
+		uint32 timestamp;
+		uint type; // 0 for string, 1 for img, 2 for video
+		bytes32 mid; // message id = keccak(sender + multihash + timestamp)
+	}
 
+	// relationship channel
+	struct Channel {
+		address user1;
+		address user2;
+		bytes32 cid; // channel id = keccak(user1 + user2)
+		bool status; // 0 for inactive, 1 for active
+		uint32 timestamp; // channel start time
+		bytes32[] msg_ids; // list of message ids
+	}
+
+	// user info
+	struct User {
+		bytes nickname;
+		bool status; // 0 for single, else 1
+		bool sex; // 0 for man, 1 for woman
+		bytes32 cid; // user's relationship channel
+	}
+
+	mapping(address => User) public users;
+	mapping(bytes32 => Channel) public channels;
+	mapping(bytes32 => Message) public msgs;
+	mapping(address => bytes32[]) public invitations; // channel invitations received
 }
